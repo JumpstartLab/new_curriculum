d49a1621e5e5307dfe7cc3ee73083d5d2642ac53
novohispano <novohispano@gmail.com>
2013-12-05 09:45:03 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewCurriculum::Application.config.secret_key_base = 'b7485e86fe7fa6eb6e57d38c8a0ea20814e96645e878c0fb1e38847e736bc5fa925ec6b80fe0345ca27806360e3017425c5c3b6606bb40d6fc9b28bab95664c6'
