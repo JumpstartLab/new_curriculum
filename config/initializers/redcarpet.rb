f13a437acbd3cb4659bdb5bf02afb09f324832d4
novohispano <novohispano@gmail.com>
2013-12-05 11:38:57 -0700

    Add markdown templating to app

---
+++ config/initializers/redcarpet.rb
@@ -0,0 +1,18 @@
+class MarkdownTemplateHandler
+  def self.call(template)
+      renderer = Redcarpet::Render::HTML.new(hard_wrap: true)
+      options  = {
+        autolink: true,
+        no_intra_emphasis: true,
+        fenced_code_blocks: true,
+        lax_html_blocks: true,
+        strikethrough: true,
+        superscript: true
+      }
+
+    Redcarpet::Markdown.new(renderer, options).render(template.source).html_safe.inspect
+  end
+end
+
+ActionView::Template.register_template_handler(:md, MarkdownTemplateHandler)
+ActionView::Template.register_template_handler(:markdown, MarkdownTemplateHandler)
