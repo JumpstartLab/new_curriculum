02ebc812f11cf0f673dbe9be08d6c8d6c1969156
novohispano <novohispano@gmail.com>
2014-01-24 11:15:37 -0700

    Moved FileIndexer to services

---
+++ app/services/indexer_service.rb
@@ -0,0 +1,67 @@
+class FileIndexer
+  attr_reader :directory, :files
+
+  def initialize(directory)
+    @directory = directory
+  end
+
+  def index_files
+    filenames
+    read_files
+    store_files
+  end
+
+  def filenames
+    Dir.foreach(directory).collect { |filename| filename if valid?(filename) }.compact!
+  end
+
+  def read_files
+    files = {}
+
+    filenames.each do |filename|
+      file = read_file(filename)
+      html = parse_markdown(file)
+
+      files["#{filename}"] = strip_html(html)
+    end
+
+    @files = files
+  end
+
+  def store_files
+    files.each do |title, content|
+      File.create(
+        title:   title
+        content: content
+        )
+    end
+  end
+
+  def read_file(filename)
+    File.open("#{directory}/#{filename}").read
+  end
+
+  def parse_markdown(file)
+    renderer = Redcarpet::Render::HTML.new(hard_wrap: true)
+    options  = {
+      autolink:           true,
+      no_intra_emphasis:  true,
+      fenced_code_blocks: true,
+      lax_html_blocks:    true,
+      strikethrough:      true,
+      superscript:        true
+    }
+
+    Redcarpet::Markdown.new(renderer, options).render(file).html_safe
+  end
+
+  def strip_html(html)
+    ActionView::Base.full_sanitizer.sanitize(html)
+  end
+
+  private
+
+  def valid?(filename)
+    filename.start_with?(".", "..") ? false : true
+  end
+end
