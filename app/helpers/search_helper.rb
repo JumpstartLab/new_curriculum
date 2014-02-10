66994e0cd2a34cf15f914f4f0d79e46491f795ac
novohispano <novohispano@gmail.com>
2014-02-10 14:44:19 -0700

    Add helpers in page and search views

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,9 @@
 module SearchHelper
+  def format_summary(content)
+    Parser.format_content(content[0..600])
+  end
+
+  def format_url(text)
+    Formatter.filename_to_url(text)
+  end
 end
--- app/services/search.rb
+++ app/services/search.rb
@@ -1,9 +1,8 @@
 class Search
   def self.pages(criteria)
     Page.search do
-      fulltext(criteria) { boost_fields title: 2.0 }
+      fulltext(criteria) { boost_fields title: 10.0 }
 
-      order_by :title, :desc
       paginate page: 1, per_page: 50
     end.results
   end
